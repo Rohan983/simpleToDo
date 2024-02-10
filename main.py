ea03ef1c6e768477b1d32e2deefb38c9e580c65e
Rohan More <rohan.more495@gmail.com>
2024-02-10 13:02:11 +0530

    Simple ToDo list in CLI

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ def showfile():
         print(f'{position + 1}: {task}', end='')
 
 
-enter_prompt = 'Choose your Action Add, Show, Edit, Remove/Complete, Close/Exit: '
+def remaction(st, ln):
+    return st[ln:]
+
+
+enter_prompt = 'Choose your Action Add, Show, Edit, Complete, Exit: '
 
 with open('files/ToDo.txt', 'r') as file:
     todos = file.readlines()
@@ -19,46 +23,44 @@ showfile()
 while True:
     action = input(enter_prompt).strip().upper()
 
-    match action:
-        case 'ADD':
-            todo = input('Enter a ToDo: ').strip().upper() + '\n'
-            todos.append(todo)
+    if action.startswith('ADD'):
+        todo = remaction(action, 3).strip().upper() + '\n'
+        todos.append(todo)
+        writefile(todos)
 
-            writefile(todos)
+    elif action == 'SHOW':
+        showfile()
 
-        case 'SHOW':
-            showfile()
+    elif action.startswith('EDIT'):
+        ed = int(remaction(action, 4).strip().upper())
+        todos[ed - 1] = input('Enter a new ToDo: ').strip().upper() + '\n'
 
-        case 'EDIT':
-            ed = int(input(f'Enter the number of ToDo to edit:'))
-            todos[ed - 1] = input('Enter a new ToDo: ').strip().upper()
+        writefile(todos)
 
-            writefile(todos)
+    elif action.startswith('COMPLETE'):
+        remC = remaction(action, 8).strip().upper()
 
-        case 'REMOVE' | 'COMPLETE':
-            remC = input(f'Enter a Number or ToDo to be {action}D: ').strip().upper()
-
-            if remC.isnumeric():
-                ind = int(remC) - 1
-                if ind < len(todos):
-                    print(f'{action[:-1]}ING {todos[ind][:-1]}')
-                    remC = todos[ind]
-                else:
-                    print('Enter valid Position')
-                    continue
-
-            if remC in todos:
-                todos.remove(remC)
-                print(f'{remC[:-1]} is {action}D')
+        if remC.isnumeric():
+            ind = int(remC) - 1
+            if ind < len(todos):
+                print(f'COMPLETING {todos[ind][:-1]}')
+                remC = todos[ind]
             else:
-                print('This ToDo does not Exist')
+                print('Enter valid Position')
+                continue
+        remC += '\n'
+        if remC in todos:
+            todos.remove(remC)
+            print(f'{remC[:-1]} is COMPLETED')
+        else:
+            print('This ToDo does not Exist')
 
-            writefile(todos)
+        writefile(todos)
 
-        case 'CLOSE' | 'EXIT':
-            break
+    elif action == 'EXIT':
+        break
 
-        case _:
-            print('Enter Valid Option')
+    else:
+        print('Enter Valid Option')
 
 print('Bye')
