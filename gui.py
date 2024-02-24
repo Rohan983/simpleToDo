a4f4b33c3c5eefdd129fe5490c973ff47865ed9a
Rohan More <rohan.more495@gmail.com>
2024-02-24 11:37:24 +0530

    added images

--- gui.py
+++ gui.py
@@ -10,13 +10,15 @@ sg.theme("Black")
 clock = sg.Text(key = 'Clock')
 add_label = sg.Text("Type in a To-Do")
 input_box = sg.InputText(tooltip="Enter To-DO", key= 'todo')
-add_button = sg.Button("Add")
+add_button = sg.Button(size=2, image_source='Images/add.png', mouseover_colors='LightBlue2',
+                       tooltip='Add ToDo', key='Add')
 
 list_box = sg.Listbox(values=functions.readfile(), key= 'todotxt',
                       enable_events=True, size=[45, 10])
 
 edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+complete_button = sg.Button(image_source='Images/complete.png', image_size= [75, 50], mouseover_colors='LightBlue2',
+                            tooltip='Add ToDo', key='Complete')
 exit_button = sg.Button("Exit")
 
 layout = [[clock],
@@ -35,15 +37,18 @@ while True:
     match event:
         case 'Add':
             todos = functions.readfile()
-            todos.append(values.get('todo') + "\n")
-            functions.writefile(todos)
-            window['todotxt'].update(values=todos)
+            if values.get('todo'):
+                todos.append(values.get('todo') + "\n")
+                functions.writefile(todos)
+                window['todotxt'].update(values=todos)
+            else:
+                sg.popup(f"Enter ToDo in Text box", font=('Helvetica', 20))
 
         case 'Edit':
             try:
                 todos = functions.readfile()
                 ind = todos.index(values.get('todotxt')[0])
-                todos[ind] = values.get('todo') + "\n"
+                todos[ind] = values.get('todo')
                 functions.writefile(todos)
                 window['todotxt'].update(values=todos)
 
