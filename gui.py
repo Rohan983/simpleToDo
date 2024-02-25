03596ca591c09e032e9eadbb48dcc776accf06b6
Rohan More <rohan.more495@gmail.com>
2024-02-25 12:35:50 +0530

    Executable added

--- gui.py
+++ gui.py
@@ -1,24 +1,29 @@
 import time
 import functions
 import PySimpleGUI as sg
+import os
+
 
 def alert(ev):
     sg.popup(f"Select a ToDo to {ev}", font=('Helvetica', 20))
+
+
+if not os.path.exists('ToDo.txt'):
+    with open("ToDo.txt", 'w') as file:
+        pass
 
 sg.theme("Black")
 
 clock = sg.Text(key = 'Clock')
 add_label = sg.Text("Type in a To-Do")
 input_box = sg.InputText(tooltip="Enter To-DO", key= 'todo')
-add_button = sg.Button(size=2, image_source='Images/add.png', mouseover_colors='LightBlue2',
-                       tooltip='Add ToDo', key='Add')
+add_button = sg.Button("Add")
 
 list_box = sg.Listbox(values=functions.readfile(), key= 'todotxt',
                       enable_events=True, size=[45, 10])
 
 edit_button = sg.Button("Edit")
-complete_button = sg.Button(image_source='Images/complete.png', image_size= [75, 50], mouseover_colors='LightBlue2',
-                            tooltip='Add ToDo', key='Complete')
+complete_button = sg.Button('Complete')
 exit_button = sg.Button("Exit")
 
 layout = [[clock],
@@ -41,6 +46,7 @@ while True:
                 todos.append(values.get('todo') + "\n")
                 functions.writefile(todos)
                 window['todotxt'].update(values=todos)
+                window['todo'].update("")
             else:
                 sg.popup(f"Enter ToDo in Text box", font=('Helvetica', 20))
 
@@ -51,12 +57,16 @@ while True:
                 todos[ind] = values.get('todo')
                 functions.writefile(todos)
                 window['todotxt'].update(values=todos)
+                window['todo'].update("")
 
             except IndexError:
                 alert('Edit')
 
         case 'todotxt':
-            window['todo'].update(value=values.get('todotxt')[0])
+            try:
+                window['todo'].update(value=values.get('todotxt')[0])
+            except IndexError:
+                pass
 
         case 'Complete':
             todos = functions.readfile()
